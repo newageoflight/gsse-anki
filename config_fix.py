efc2ca501d87bb25b5721f67a7a287ba47898fb6
Chris Chen <33755603+newageoflight@users.noreply.github.com>
2022-02-21 01:01:27 +1100

    Updates deck; removes media

--- config_fix.py
+++
@@ -1,9 +0,0 @@
-import json
-
-aberrant_key = "3b2afa19-3f35-11eb-ba0b-4c1d96d9eed1"
-
-# Whatever
-with open("Anki_for_GSSE/deck.json") as ifp:
-    deck_json = json.load(ifp)
-
-
--- resource-shortlist.md
+++
@@ -1,3 +0,0 @@
-# Highest yield resources for the GSSE
-
-See [Google Docs](https://docs.google.com/document/d/1u0VH5QFWejQJ6JB6Fr3EUFNo6QLLc57ddutPTaIz_P8/edit#)
